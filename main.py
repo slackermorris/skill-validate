1841e3e30051da90eaa4335aaa0cb8f32536857e
slackermorris <morris.w.jack@gmail.com>
2026-03-11 12:45:38 +1300

    feature: use standard lib for validating CLI args

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import sys
+import argparse
 
-def main (args=None):
-    # Extract arguments from the CLI but support the possibility that this function is called elsewhere.
-    if args is None:
-            args = sys.argv[1:]
-            print(f"Arguments of the script:, {args}=")
+
+
+def init_argparse() -> argparse.ArgumentParser:
+     parser = argparse.ArgumentParser(
+          description="Perform 'health' checks on an Agent Skill"
+     )
+
+     parser.add_argument('-a', '--action', choices=["usage", "fix"], required=True)
+     parser.add_argument('skill_name')
+
+     return parser
+
+def main ():
+    print('Start of function')
+
+    parser = init_argparse()
+
+    args = parser.parse_args()
+
+    print(f"Arguments of the script:, {args}=")
+
+    print('End of function')
             
 
 main()
